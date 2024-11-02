b4f4404566751ff9b2f63144bc81c01369f783f4
Nikos Kleidopoulos <nikos.kleidopoulos@helloworld.com.au>
2024-11-02 15:19:19 +0200

    Spring boot initial commit

---
+++ src/main/java/com/nklido/garminapi/GarminapiApplication.java
@@ -0,0 +1,13 @@
+package com.nklido.garminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarminapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarminapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nklido/garminapi/GarminapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nklido.garminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarminapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
